3b4a394d307f11e08dcf13f2f45810648c74b176
Eric Vollenweider <evollenwe@ethz.ch>
2021-12-12 20:20:06 +0100

    Merge tensorboard logs

--- src/plotter.py
+++ src/plotter.py
@@ -1,30 +1,64 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import matplotlib
 
 class Plotter:
-    def __init__(self, title):
+    def __init__(self, transparent = True, sub_plot = False, rows = 1, cols = 1):
+        self.transparent = transparent
+
+        self.subplot = sub_plot
+        if sub_plot:
+            self.rows = rows
+            self.cols = cols
+            self.fig, self.ax = plt.subplots(self.rows, self.cols)
+            self.current_subplot_index = 0
+        else:
+            self.fig, self.ax = plt.subplot(1,1)
+
+    def clear(self):
         plt.figure().clear()
         plt.close()
         plt.cla()
         plt.clf()
-        plt.title(title)
 
-    def plot_raw(self, data):
-        plt.plot(data, 'lightgray')
+    def get_ax(self):
+        if self.subplot:
+            row, col = int(self.current_subplot_index / self.rows), self.current_subplot_index % self.cols
+            return self.ax[row,col]
+        else:
+            return self.ax
 
-    def plot_smooth(self, data):
-        plt.plot(data, 'black')
+    def plot_raw(self, data, color = 'lightgray', title = ''):
+        self.get_ax().plot(data, color)
+        self.set_title(title)
 
-    def plot_region(self, mins, maxs):
-        plt.fill_between(np.arange(0, mins.shape[0]), mins, maxs, color='lightgray')
+    def plot_smooth(self, data, color = 'black', title = ''):
+        self.get_ax().plot(data, color)
+        self.set_title(title)
+
+    def plot_region(self, mins, maxs, color = 'lightgray', title = ''):
+        c = matplotlib.colors.to_rgba(color, alpha = 0.3)
+        self.get_ax().fill_between(np.arange(0, mins.shape[0]), mins, maxs, color=c)
+        self.set_title(title)
+
+    def set_title(self, title):
+        if title != '':
+            self.get_ax().title.set_text(title)
+
+    def advance_subplot(self):
+        self.current_subplot_index += 1
+
+    def legend(self, items):
+        handles = self.get_ax().get_lines()
+        plt.gcf().legend(handles, items, loc='upper center')
 
     def save(self, path):
         img_path = os.path.join(os.path.dirname(__file__), 'output', path)
         img_dir = os.path.dirname(img_path)
         if not os.path.isdir(img_dir):
             os.mkdir(os.path.dirname(img_path))
-        plt.savefig(img_path, format = 'png', transparent = True)
+        plt.savefig(img_path, format = 'png', transparent = self.transparent)
 
     def show(self):
         plt.show()
--- src/tensorboard_plotter.py
+++ src/tensorboard_plotter.py
@@ -1,38 +1,77 @@
 
+from matplotlib.cbook import index_of
 from plotter import Plotter
 from tensorboard_reader import TensorboardReader
 import yaml
+import matplotlib.pyplot as plt
 import os
 
 class TensorboardPlotter:
-    def __init__(self, name_map_file):
+    def __init__(self, config_file):
+        plt.rcParams['figure.figsize'] = (10,7)
+        plt.rcParams['figure.subplot.hspace'] = 0.35
+        plt.rcParams['figure.subplot.wspace'] = 0.22
         self.reader = TensorboardReader()
-        with open(os.path.dirname(__file__) + "/" + name_map_file, 'r') as stream:
+        with open(os.path.dirname(__file__) + "/" + config_file, 'r') as stream:
             self.cfg = yaml.safe_load(stream)
+        with open(os.path.dirname(__file__) + "/cfg/name_map.yaml", 'r') as stream:
+            self.name_map = yaml.safe_load(stream)['name_map']
 
-        self.name_map = self.cfg['name_map']
 
-    def plot_all(self, file):
-        tags = self.reader.get_tag_names(file)
+        self.rows = self.cfg['plot']['subplot']['rows'] 
+        self.cols = self.cfg['plot']['subplot']['cols'] 
+        self.subplot = self.rows > 1 or self.cols > 1
+
+    def plot(self, files):
+        tags = []
+
+        # find the set of tags that are included in all files
+        for file in files:
+            t = self.reader.get_tag_names(file)
+            if tags == []:
+                tags = t
+            else:
+                for tag in tags:
+                    if tag not in tags:
+                        tags.remove(tag)
 
         if 'white_list' in self.cfg:
             tags = [tag for tag in tags if tag in self.cfg['white_list']]
         if 'black_list' in self.cfg:
             tags = [tag for tag in tags if tag not in self.cfg['black_list']]
 
+        colors = ['blue', 'orange']
+        backgrounds = ['lightsteelblue', 'bisque']
+
+        if self.subplot:
+            p = Plotter(transparent= False, sub_plot = self.subplot, rows = self.rows, cols = self.cols)
+
         for tag in tags:
-            
-            raw, smoothed = self.reader.extract_from_file(file, tag)
+            raw = []
+            smoothed = []
+            for file in files:
+                r, s = self.reader.extract_from_file(file, tag)
+                raw.append(r)
+                smoothed.append(s)
 
             
             title = tag
             if tag in self.name_map:
                 title = self.name_map[tag]
-            
-            p = Plotter(title)
-            p.plot_smooth(smoothed)
-            mi,ma = self.reader.calc_region(raw, 30)
-            p.plot_region(mi, ma)
 
-            print(f"Saving {tag} into {tag}.png")
-            p.save(f"{tag}.png")
+            
+            for i in range(len(raw)):
+                p.plot_smooth(smoothed[i], colors[i], title=title)
+                mi,ma = self.reader.calc_region(raw[i], 30)
+                p.plot_region(mi, ma, backgrounds[i])
+
+            if not self.subplot:
+                p.save(f"{tag}_combined.png")
+                p.clear()
+            else:
+                if not tag == tags[-1]:
+                    p.advance_subplot()
+
+        if self.subplot:
+            p.legend(self.cfg['plot']['legend'])
+            p.save(f"{self.cfg['plot']['title']}.png")
